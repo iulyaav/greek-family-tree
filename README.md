f51fe207983dc4f6b2c67e5b24a50fc1b84c6244
iulyaav <avram.iulia.augusta@gmail.com>
2021-01-13 09:26:53 +0000

    Refresh the site and add some logic

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # greek-family-tree
 Pet project to learn about CSS and pretty colors
+
+
+Using Node 10.9.0 and Foundation
+
+To start -> `npm start`
--- olympus/js/app.js
+++ olympus/js/app.js
@@ -1,1 +1,22 @@
 $(document).foundation();
+
+var gods = [{
+    "name": "Zeus",
+    "img": "/img/zeus.png"
+},
+{
+    "name": "Hera",
+    "img": "/img/hera.png"
+}]
+
+var output = "<div>";
+$.each(gods, function(index, god) {
+
+    var image = "<img src=\"" + god.img + "\" class=\"god-logo\"/>"
+    output += "<div class=\"god-" + god.name + "\">" + god.name + image + "</div>"
+});
+
+var image = "<img src=\"/img/zeus.png\"/>";
+output += "</div>"
+
+$(".gods").html(output);
