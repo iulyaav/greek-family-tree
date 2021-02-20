76f23f86554ed1170dc8752aa5344464d43df60b
iulyaav <avram.iulia.augusta@gmail.com>
2021-02-20 12:55:06 +0000

    Add panels and some style

--- olympus/src/components/panel/index.js
+++ olympus/src/components/panel/index.js
@@ -2,8 +2,10 @@ import Profile from '../profile';
 import style from './style.css';
 
 const Panel = ({ children, ...props }) => (
-	<div>
+	<div class={style.panel}>
+        <div class={style.arrows}><i class="fa fa-chevron-left fa-2x"/></div>
         <Profile {...props} />
+        <div class={style.arrows}><i class="fa fa-chevron-right fa-2x"/></div>
     </div>
 );
 
--- olympus/src/components/profile/index.js
+++ olympus/src/components/profile/index.js
@@ -2,10 +2,10 @@ import style from './style.css';
 
 const Profile = ({ children, ...props })  => (
 	<div class={style.profilebox}>
-        <h1>{props.name}</h1>
         <div>
             <img class={style.icon} src={props.icon}/>
         </div>
+        <h1>{props.name}</h1>
     </div>
     
 );
